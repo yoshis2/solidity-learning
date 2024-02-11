01130df0faae899b1d53b688308c11e2683aa8af
yoshis2 <seki@threenext.com>
2024-02-11 10:49:08 +0900

    rename file

--- tutorial/02-pureview.sol
+++ tutorial/02-pureview.sol
@@ -2,18 +2,14 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-contract Hello{
-    uint hoge = 3;
-
-    function hello() public pure returns(string memory) {
-        return "Hello World";
-    }
+contract PureView{
+    uint param = 3;
     
     function add(uint x, uint y) public pure returns(uint){
         return x + y;
     }
 
     function addHoge(uint x, uint y) public view returns (uint) {
-        return x + y + hoge;
+        return x + y + param;
     }
 }
