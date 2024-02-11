7154b3b5a077fbb0978437d1a3042f7124a359ae
yoshis2 <seki@threenext.com>
2024-02-11 23:08:25 +0900

    add array2

---
+++ tutorial/01-hello.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract HelloWorld{
+    function hello() public pure returns(string memory) {
+        return "Hello World";
+    }
+ }
