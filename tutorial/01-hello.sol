074d49baa8eb5f63068a0a667fb12da50b0e28a7
yoshis2 <seki@threenext.com>
2024-02-11 23:10:29 +0900

    delete oldfile

--- tutorial/01-hello.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >=0.8.2 <0.9.0;
-
-contract HelloWorld{
-    function hello() public pure returns(string memory) {
-        return "Hello World";
-    }
- }
